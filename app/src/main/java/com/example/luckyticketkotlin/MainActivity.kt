dc13ea94d15e236065b34f399f8d910825788409
Ihor Shevchuk <bluesjazz666@gmail.com>
2021-09-06 18:38:18 +0300

    Edited inputType to number and maxLength to 6 for EditTex; Created fun getResult; Created fun sumNumber: Edited some errors;

--- app/src/main/java/com/example/luckyticketkotlin/MainActivity.kt
+++ app/src/main/java/com/example/luckyticketkotlin/MainActivity.kt
@@ -10,9 +10,11 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
 
-       override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
@@ -20,48 +22,38 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("UseCompatLoadingForDrawables")
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    fun onClick(view: View) {
-
-        val max = 6
-        val etNumber = findViewById<EditText>(R.id.et_ticket_number).text
+    fun onClick(view: View)
+    {
+        val etNumber = findViewById<EditText>(R.id.et_ticket_number).text.toString()
         val bulb = findViewById<ImageView>(R.id.iv_image)
+        val resultTicket: String
 
-        if (etNumber.length == max) {
-
-            try {
-                val resultTicket = etNumber.toList().map { it.toString().toInt() }
-                val firstPart = resultTicket.slice(0..2).reduce{total, next -> total + next}
-                val secondPart = resultTicket.slice(3..5).reduce{total, next -> total + next}
-
-                if(firstPart == secondPart)
-                {
-                    bulb.setImageResource(R.drawable.green_bulb)
-                }
-                else
-                    {
-                        bulb.setImageResource(R.drawable.red_bulb)
-                    }
-                }
-            catch (e: NumberFormatException)
-                {
-                    Toast.makeText(
-                        applicationContext,
-                        "Enter your ticket number",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                                     }
-        else
+        if (etNumber.length != 6)
+        {
+            bulb.setImageResource(R.drawable.grey_bulb)
+            Toast.makeText(applicationContext, "Minimum number of characters 6",
+                Toast.LENGTH_SHORT).show()
+        } else
+        {
+            if (getResult(etNumber))
             {
-                bulb.setImageResource(R.drawable.grey_bulb)
-                Toast.makeText(
-                    applicationContext,
-                    "Minimum number of characters 6",
-                    Toast.LENGTH_SHORT
-                ).show()
+                bulb.setImageResource(R.drawable.green_bulb)
+            } else
+            {
+                bulb.setImageResource(R.drawable.red_bulb)
             }
+        }
+    }
 
+    private fun getResult(num: String): Boolean
+    {
+        return sumNumber(num, 0) == sumNumber(num, num.length - 3)
+    }
 
-
+    private fun sumNumber(num: String, index: Int): Int
+    {
+        var sum = 0
+        for (i in index until index + 3) sum += Character.digit(num[i], 10)
+        return sum
     }
 }
